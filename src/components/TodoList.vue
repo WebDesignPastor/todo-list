7ed6c15641d7182c0babec0f4f42d8b59e29b3f9
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-07-04 09:30:49 +0200

    Finished the todo list

---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>My Todo List</h1>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+      </li>
+    </ul>
+    <input type="text" v-model="newTodo" />
+    <button @click="addTodo">Add Todo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: "",
+      todos: ["Learn Vue", "Practice Vue", "Master Vue"],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(this.newTodo);
+      this.newTodo = "";
+    },
+  },
+};
+</script>
